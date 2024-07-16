b822ca35e2dcc73fc12158dbac5102e022302d83
hashir1111 <110687639+hashir1111@users.noreply.github.com>
2024-07-16 17:33:07 +0500

    utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
-
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`app listening on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database Connection error: ${err}`);
+  });
---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,5 @@
+const AsyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
