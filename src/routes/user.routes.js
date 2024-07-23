d228a19d805e3a4b0f34dc11480cd2f2ec64de5e
hashir1111 <110687639+hashir1111@users.noreply.github.com>
2024-07-23 20:00:42 +0500

    user route for updations added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import {
   accessRefreshToken,
+  getCurrentUser,
   registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  updatePassword,
   userLogin,
   userLogut,
 } from "../controllers/user.controller.js";
@@ -26,5 +31,12 @@ router.route("/register").post(
 router.route("/login").post(userLogin);
 router.route("/logout").post(verifyJWT, userLogut);
 router.route("/refresh_token").post(accessRefreshToken);
+router.route("/updatePassword").post(verifyJWT, updatePassword);
+router.route("/currentUser").get(verifyJWT, getCurrentUser);
+router.route("updateAccount").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
 export default router;
